830a659fba07086bc92b321211538f96f3b199d0
Jackeagle <jackeagle102@gmail.com>
2018-04-22 07:40:53 -0400

    newinstaller: Move build colors definations to separate bliss folder
    Change-Id: I78fae4b82eba3b12db6a66c390faf3a0f8c947b4
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- Android.mk
+++ Android.mk
@@ -36,24 +36,8 @@ define build-squashfs-target
 endef
 endif
 
-CLR_RED=$(shell tput setaf 1)
-CLR_GRN=$(shell tput setaf 2)
-CLR_YLW=$(shell tput setaf 3)
-CLR_BLU=$(shell tput setaf 4)
-CLR_MAG=$(shell tput setaf 5)
-CLR_CYN=$(shell tput setaf 6)
-CLR_WHT=$(shell tput setaf 7)
-CLR_BOLD=$(shell tput bold)
-CLR_RST=$(shell tput sgr0)
-
-# Shell color defs:
-CL_RED="\033[31m"
-CL_GRN="\033[32m"
-CL_YLW="\033[33m"
-CL_BLU="\033[34m"
-CL_MAG="\033[35m"
-CL_CYN="\033[36m"
-CL_RST="\033[0m"
+# Bliss Build Colors
+include $(LOCAL_PATH)/bliss/bliss_colors.mk
 
 initrd_dir := $(LOCAL_PATH)/initrd
 initrd_bin := \
